8d1bbe8d00aab3da5f488e53281ea02b206d8bee
yadavsuraj999 <yadavsuraj8425@gmail.com>
2024-10-22 16:54:43 +0530

    start pointer

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main(){
     int y=5;
     int *pointer = &y;
+    int **pointertopointer = &pointer;
     cout<<pointer<<endl;  //--->this is for were y  adderss
     cout<<*pointer<<endl;  // ---> this is y value
+    cout<<**pointertopointer<<endl;
     cout<<&y;
 return 0;
 }
